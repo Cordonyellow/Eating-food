0c315e70d52c635c27a63701dd3b0e2f03839a3d
hueychen27 <hueychen27@outlook.com>
2022-11-08 20:29:41 -0800

    Added eating Github

--- Eating-food.py
+++ Eating-food.py
@@ -5,6 +5,8 @@ while True:
         break
     elif food == 'Cordonyellow' or food == 'cordonyellow':
         print('Do not eat the creator of this program!')
+    elif food == 'Github' or food == 'github':
+		print('You don\'t like Github? :(')
     else:
         Eaten_food.append(food)
     print('You have eaten', Eaten_food)
